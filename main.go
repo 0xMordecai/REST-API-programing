2487d94a2879f29b91d99a4116e47a86a848ce76
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-11-09 23:06:04 +0100

    add

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 const (
 	header = `<!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>Markdown Preview Tool</title>
-</head>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>Markdown Preview Tool</title>
+	</head>
 <body>
 `
 	footer = `
