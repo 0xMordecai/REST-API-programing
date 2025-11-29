b2421fd3b645c16dd629a7073cf2e4e4bff7ca66
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-11-29 23:57:16 +0100

    add

--- myclient/main.go
+++ myclient/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
 	result, err := http.Get("http://localhost:8888")
 	if err != nil {
 		panic(err)
 	}
+	defer result.Body.Close()
+	data, err := io.ReadAll(result.Body)
 }
