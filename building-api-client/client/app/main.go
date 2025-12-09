fae150c92d89367828b2fac2c9ac1b6191527450
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-12-09 21:36:21 +0100

    Add an application that uses the API client that we developed

--- building-api-client/client/app/main.go
+++ building-api-client/client/app/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+
+	"github.com/kings5layer/API-PROGRAMING/building-api-client/client"
+)
+
+func main() {
+	c := client.NewClient("http://localhost:8888")
+	if err := c.Login("user", "password"); err != nil {
+		log.Fatal(err)
+	}
+
+}
